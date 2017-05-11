ee11d7c0e00dca64956b3f22b9bab36ff767163c
Fatih BAKIR <mfatihbakir@gmail.com>
2017-05-12 00:56:25 +0300

    viewport updates

--- rtk/include/rtk/utility.hpp
+++ rtk/include/rtk/utility.hpp
@@ -6,10 +6,12 @@
 #define RTK_UTILITY_HPP_HPP
 
 #include <rtk/graphics/rect.hpp>
+#include <glm/vec2.hpp>
 
 namespace RTK_NAMESPACE
 {
     void set_viewport(screen_rect rect);
+    screen_rect get_rect(const resolution& res, const glm::vec2& vp_pos, const glm::vec2& vp_size);
 }
 
 #endif //RTK_UTILITY_HPP_HPP
--- rtk/utility.cpp
+++ rtk/utility.cpp
@@ -11,5 +11,12 @@ namespace RTK_NAMESPACE
     {
         glViewport(rect.m_position.x, rect.m_position.y, rect.m_size.width, rect.m_size.height);
     }
+
+    screen_rect get_rect(const resolution& res, const glm::vec2& vp_pos, const glm::vec2& vp_size)
+    {
+        rtk::size<rtk::pixels> sz{ rtk::pixels(res.width * vp_size.x), rtk::pixels(res.height * vp_size.y) };
+        rtk::point2d<rtk::pixels> pos{ rtk::pixels(res.width * vp_pos.x), rtk::pixels(res.height * vp_pos.y) };
+        return {pos, sz};
+    }
 }
 
