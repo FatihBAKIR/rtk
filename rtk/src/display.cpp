e12a3cd46024615fc5b608830d68b1cc908127bc
Fatih BAKIR <mfatihbakir@gmail.com>
2017-05-21 15:03:42 +0300

    rtf namespace

--- rtk/src/display.cpp
+++ rtk/src/display.cpp
@@ -5,41 +5,42 @@
 #include <rtk/display.hpp>
 #include <GLFW/glfw3.h>
 
-
-std::vector<rtk::display> rtk::get_displays() {
-    int mon_c;
-    auto monitors = glfwGetMonitors(&mon_c);
-
-    std::vector<rtk::display> disps;
-    for (int i = 0; i < mon_c; ++i)
-    {
-        disps.push_back(monitors[i]);
-    }
-    return disps;
-}
-
-rtk::display rtk::get_primary_display() {
-    return { glfwGetPrimaryMonitor() };
-}
-
-rtk::display::display(GLFWmonitor* mon) :
-    opaque(mon)
+namespace RTK_NAMESPACE
 {
+    std::vector<display> get_displays() {
+        int mon_c;
+        auto monitors = glfwGetMonitors(&mon_c);
+
+        std::vector<display> disps;
+        for (int i = 0; i < mon_c; ++i)
+        {
+            disps.push_back(monitors[i]);
+        }
+        return disps;
+    }
+
+    display get_primary_display() {
+        return { glfwGetPrimaryMonitor() };
+    }
+
+    display::display(GLFWmonitor* mon) :
+        opaque(mon)
+    {
+    }
+
+    const char *display::get_name() const {
+        return glfwGetMonitorName(opaque);
+    }
+
+    physical_size display::get_physical_size() const {
+        int w, h;
+        glfwGetMonitorPhysicalSize(opaque, &w, &h);
+        return physical_size(millimeters(w), millimeters(h));
+    }
+
+    resolution display::get_resolution() const {
+        auto mode = glfwGetVideoMode(opaque);
+
+        return resolution(pixels(mode->width), pixels(mode->height));
+    }
 }
-
-const char *rtk::display::get_name() const {
-    return glfwGetMonitorName(opaque);
-}
-
-rtk::physical_size rtk::display::get_physical_size() const {
-    int w, h;
-    glfwGetMonitorPhysicalSize(opaque, &w, &h);
-    return rtk::physical_size(rtk::millimeters(w), rtk::millimeters(h));
-}
-
-rtk::resolution rtk::display::get_resolution() const {
-    auto mode = glfwGetVideoMode(opaque);
-
-    return rtk::resolution(rtk::pixels(mode->width), rtk::pixels(mode->height));
-}
-
